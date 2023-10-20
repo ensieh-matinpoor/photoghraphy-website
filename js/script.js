80f02f5b77cfaf0e27e4a01feef02e06d2f123f6
Ensieh Matinpoor <ensieh72@gmail.com>
2023-10-20 17:42:43 +0100

    fix close icon

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const showMenuButton = document.getElementById("show-menu-button");
 const menu = document.getElementById("menu");
+const closeIcon= document.getElementById("close-icon");
 
 showMenuButton.addEventListener("click", function () {
   menu.style.display="block";
-
 });
 
 
+closeIcon.addEventListener("click", function (){
+  menu.style.display="none";
+});
