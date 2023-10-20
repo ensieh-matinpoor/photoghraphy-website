69deaa60605429d643000e1b5dfd8b1e2b677b13
Ensieh Matinpoor <ensieh72@gmail.com>
2023-10-20 15:40:16 +0100

    fixed js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-console.log("Hello world!");
-
 const showMenuButton = document.getElementById("show-menu-button");
 const menu = document.getElementById("menu");
 
 showMenuButton.addEventListener("click", function () {
-  menu.classList.toggle("open");
+  menu.style.display="block";
+
 });
 
 
