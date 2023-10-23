018ed39917214fa5adaaaf7f0d71f271f2f70ef1
Ensieh Matinpoor <ensieh72@gmail.com>
2023-10-23 15:31:12 +0100

    fix menu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 const showMenuButton = document.getElementById("show-menu-button");
 const menu = document.getElementById("menu");
 const closeIcon= document.getElementById("close-icon");
+const main= document.getElementById("main");
 
 showMenuButton.addEventListener("click", function () {
+  main.style.filter="blur(5px)"
   menu.style.display="block";
 });
 
 
 closeIcon.addEventListener("click", function (){
+  main.style.filter="none";
   menu.style.display="none";
 });
